35fc11939805c5dd48b582b20d14949132ddd646
Espen Klem <espen.klem@gmail.com>
2018-04-12 18:10:16 +0200

    First working version for crawling all URL's on a Wikipedia-site for one language

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wikipedia-stopword-crawler
-A Wikipedia text crawler to create stopword lists for any language in the world.
+
+A Wikipedia text crawler to create [stopword](https://github.com/fergiemcdowall/stopword) lists for any language in the world.
+
+Crawl all main pages and extract text for a stopword analysis by [stopword-trainer](https://github.com/eklem/stopword-trainer).
+
+Developed by [Espen Klem](mailto:espen.klem@gmail.com)
---
+++ urls-no.js
@@ -0,0 +1,58 @@
+// Libraries
+const nc = require('norch-crawlers');
+const request = require('request')
+const cheerio = require('cheerio')
+
+
+// Variables
+let domain = 'http://no.wikipedia.org'
+let startURL = 'https://no.wikipedia.org/w/index.php?title=Spesial:Alle_sider&from='
+let urls = []
+const file = 'wikipedia-no-urls.json'
+const headerOptions = {
+  headers: {
+    'User-Agent': 'NorchCrawlersBot/0.0.3 (+https://eklem.github.io/wikipedia-github-crawlers/)',
+    'from': 'espen.klem@gmail.com'
+  }
+}
+
+
+crawlUrls(startURL, urls)
+
+function crawlUrls(requestUrl, urls) {
+  request(requestUrl, headerOptions, function (error, response, body) {
+    if (error) {
+      console.log('error:', error);
+    }
+    console.log('statusCode:', response && response.statusCode);
+
+    // Get content and push to array urls[]
+    let $ = cheerio.load(body)
+    $('div.mw-allpages-body ul.mw-allpages-chunk li a').each(function (index, element) {
+      if (!$(element).hasClass('mw-redirect')) {
+        urls.push(domain + $(element).attr('href'))
+      }
+    })
+
+    let nextPage = ''
+    var nextPageExists = 0
+    $('div.mw-allpages-nav a').each(function (index, element) {
+      if (nc.matchStringToPattern($(element),'Neste side') && nextPage === '') {
+        nextPageExists++
+        nextPage = domain + $(element).attr('href')
+      }
+    })
+    console.log('next page: ' + nextPage)
+    console.log('next pag exists: ' + nextPageExists)
+
+    if (nextPageExists > 0) {
+      nc.playNice(2000).then(() => {
+        crawlUrls(nextPage, urls)
+      })
+    } else if (nextPageExists === 0) {
+      console.log(nextPage)
+      // Write to file
+      nc.writeJSON(urls, file)
+    }
+  })
+}
