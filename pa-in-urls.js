f099e51f34da909c60d3bf87aa37ca64860cc8c1
Espen Klem <espen.klem@gmail.com>
2018-09-25 07:23:15 +0200

    Added the start of Punjabi gurmukhi Wikipedia crawler

---
+++ pa-in-urls.js
@@ -0,0 +1,59 @@
+// Libraries
+const nc = require('norch-crawlers');
+const request = require('request')
+const cheerio = require('cheerio')
+
+
+// Variables
+let domain = 'https://pa.wikipedia.org/'
+let startURL = 'https://pa.wikipedia.org/w/index.php?title=%E0%A8%96%E0%A8%BC%E0%A8%BE%E0%A8%B8:%E0%A8%B8%E0%A8%BE%E0%A8%B0%E0%A9%87_%E0%A8%B8%E0%A8%AB%E0%A8%BC%E0%A9%87&hiderਅਗਲਾ ਸਫ਼ਾ1'
+let urls = []
+const file = 'pa-in-urls.json'
+const headerOptions = {
+  headers: {
+    'User-Agent': 'NorchCrawlersBot/0.0.3 (+https://eklem.github.io/wikipedia-github-crawlers/)',
+    'from': 'espen.klem@gmail.com'
+  }
+}
+
+
+crawlUrls(startURL, urls)
+
+function crawlUrls(requestUrl, urls) {
+  request(requestUrl, headerOptions, function (error, response, body) {
+    if (error) {
+      console.log('error:', error);
+    }
+    console.log('Page crawled: ' + requestUrl)
+    console.log('statusCode:', response && response.statusCode);
+
+    // Get content and push to array urls[]
+    let $ = cheerio.load(body)
+    $('div.mw-allpages-body ul.mw-allpages-chunk li a').each(function (index, element) {
+      if (!$(element).hasClass('mw-redirect')) {
+        urls.push(domain + $(element).attr('href'))
+      }
+    })
+
+    let nextPage = ''
+    var nextPageExists = 0
+    $('div.mw-allpages-nav a').each(function (index, element) {
+      if (nc.matchStringToPattern($(element),'ਅਗਲਾ ਸਫ਼ਾ') && nextPage === '') {
+        nextPageExists++
+        nextPage = domain + $(element).attr('href')
+      }
+    })
+    console.log('next page: ' + nextPage)
+    console.log('next pag exists: ' + nextPageExists)
+
+    if (nextPageExists > 0) {
+      nc.playNice(2000).then(() => {
+        crawlUrls(nextPage, urls)
+      })
+    } else if (nextPageExists === 0) {
+      console.log(nextPage)
+      // Write to file
+      nc.writeJSON(urls, file)
+    }
+  })
+}
